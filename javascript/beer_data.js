ad32769e11a8f3e8b229ecdfab7d8f2ed02a772b
spuddaemon <spuddaemon@gmail.com>
2016-12-12 22:53:40 -0800

    Add files via upload

---
+++ javascript/beer_data.js
@@ -0,0 +1,78 @@
+
+var jsonBeer = {
+    "status":"2012",
+	"category":[
+		{"style":"All", "details": [
+			{"brewery":"Russion River Brewing Compnay","name":"Pliny The Younger","style":"Imperial IPA","ABV":"11.00%","rating":"4.67"},
+			{"brewery":"Brouwerij Westvleteren","name":"Trappist Westvletern 12","style":"Quadrupel","ABV":"10.20%","rating":"4.64"},
+			{"brewery":"The Alchemist","name":"Heady Topper","style":"Imperial IPA","ABV":"8.00%","rating":"4.61"},
+			{"brewery":"Founders Brewing Company","name":"Founders CBS Imperial Stout","style":"Imperial Stout","ABV":"10.60%","rating":"4.6"},
+			{"brewery":"Russion River Brewing Compnay","name":"Pliny The Elder","style":"Imperial IPA","ABV":"8.00%","rating":"4.59"},
+			{"brewery":"Three Floyds Brewing Co.","name":"Bourbon Barrel Aged Vanilla Bean Dark Lord","style":"Russian Imperial Stout","ABV":"15.00%","rating":"4.59"},
+			{"brewery":"Goose Island Beer Co.","name":"King Henry","style":"English Barleywine","ABV":"13.40%","rating":"4.56"},
+			{"brewery":"Kern River Brewing Company","name":"Citra DIPA","style":"Imperial IPA","ABV":"8.00%","rating":"4.55"},
+			{"brewery":"Narke Kulturbryggeri AB","name":"Kaggen! Stormaktsporter","style":"Russian Imperial Stout","ABV":"10.00%","rating":"4.55"},
+			{"brewery":"Founders Brewing Company","name":"Founders KBS (Kentucky Breakfast Stout)","style":"Imperial Stout","ABV":"11.20%","rating":"4.54"}
+		]},
+		{"style":"American IPA", "details": [
+			{"brewery":"Ballast Point Brewing Compnay","name":"Sculpin India Pale Ale","style":"American IPA","ABV":"7.00%","rating":"4.4"},
+			{"brewery":"Minneapolis Town Hall Brewery","name":"Masala Mama India Pale Ale","style":"American IPA","ABV":"6.00%","rating":"4.39"},
+			{"brewery":"Surly Brewing Company","name":"Furious","style":"American IPA","ABV":"6.20%","rating":"4.34"},
+			{"brewery":"Fat Heads Brewery &amp; Saloon","name":"Fat Head's Head Hunter IPA","style":"American IPA","ABV":"7.50%","rating":"4.34"},
+			{"brewery":"Alpine Beer Company","name":"Nelson","style":"American IPA","ABV":"7.10%","rating":"4.33"},
+			{"brewery":"Selin's Grove Brewing Company","name":"India Pale Ale","style":"American IPA","ABV":"7.00%","rating":"4.32"},
+			{"brewery":"Alpine Beer Company","name":"Duet IPA","style":"American IPA","ABV":"7.00%","rating":"4.31"},
+			{"brewery":"Russian River Brewing Company","name":"Blind Pig IPA","style":"American IPA","ABV":"6.10%","rating":"4.3"},
+			{"brewery":"Stone Brewing Co.","name":"IPA - Double Dry Hopped","style":"American IPA","ABV":"6.90%","rating":"4.3"},
+			{"brewery":"AleSmith Brewing Company","name":"AleSmith IPA","style":"American IPA","ABV":"7.25%","rating":"4.29"}
+		]},
+		{"style":"American Pale Ale", "details": [
+			{"brewery":"Three Floyds Brewing Co.","name":"Zombie Dust","style":"American Pale Ale","ABV":"6.40%","rating":"4.51"},
+			{"brewery":"Alpine Beer Company","name":"Hoppy Birthday","style":"American Pale Ale","ABV":"5.25%","rating":"4.44"},
+			{"brewery":"Hill Farmstead Brewery","name":"Edward","style":"American Pale Ale","ABV":"5.20%","rating":"4.3"},
+			{"brewery":"Hill Farmstead Brewery","name":"What Is Enlightenment?","style":"American Pale Ale","ABV":"5.40%","rating":"4.3"},
+			{"brewery":"Three Floyds Brewing Co.","name":"Alpha King Pale Ale","style":"American Pale Ale","ABV":"6.00%","rating":"4.28"},
+			{"brewery":"Russian River Brewing Company","name":"Row 2/Hill 56","style":"American Pale Ale","ABV":"5.80%","rating":"4.27"},
+			{"brewery":"Hill Farmstead Brewery","name":"Citra Pale Ale","style":"American Pale Ale","ABV":"5.40%","rating":"4.21"},
+			{"brewery":"Half Acre Beer Company","name":"Daisy Cutter Pale Ale","style":"American Pale Ale","ABV":"5.20%","rating":"4.18"},
+			{"brewery":"Maine Beer Company","name":"MO (Madeline &amp; Oliver) Pale Ale","style":"American Pale Ale","ABV":"6.00%","rating":"4.13"},
+			{"brewery":"Russian River Brewing Company","name":"Parking Violation","style":"American Pale Ale","ABV":"5.50%","rating":"4.13"}
+		]},
+		{"style":"American Stout", "details": [
+			{"brewery":"Rogue Ales","name":"Chocolate Stout","style":"American Stout","ABV":"6.00%","rating":"4.22"},
+			{"brewery":"Deschutes Brewery","name":"Obsidian Stout","style":"American Stout","ABV":"6.40%","rating":"4.21"},
+			{"brewery":"Evolution Craft Brewing Company","name":"Rise Up Stout","style":"American Stout","ABV":"6.80%","rating":"4.21"},
+			{"brewery":"Brasserie Dieu Du Ciel","name":"Aphrodisiaque","style":"American Stout","ABV":"6.50%","rating":"4.19"},
+			{"brewery":"Bullfrog Brewery","name":"Double Coffee Stout","style":"American Stout","ABV":"7.30%","rating":"4.14"},
+			{"brewery":"Jackie O's Pub &amp; Brewery","name":"Java The Stout","style":"American Stout","ABV":"6.50%","rating":"4.14"},
+			{"brewery":"Triumph Brewing Company","name":"Coffee &amp; Cream Stout","style":"American Stout","ABV":"5.60%","rating":"4.13"},
+			{"brewery":"Willimantic Brewing Co.","name":"Willimantic Pony Espresso Stout","style":"American Stout","ABV":"5.80%","rating":"4.13"},
+			{"brewery":"Great River Brewery","name":"Redband Stout","style":"American Stout","ABV":"6.00%","rating":"4.11"},
+			{"brewery":"Ale Asylum","name":"Big Slick Stout","style":"American Stout","ABV":"7.00%","rating":"4.11"}
+		]},
+		{"style":"Belgian Pale Ale", "details": [
+			{"brewery":"Brouwerij Westvleteren","name":"Trappist Westvleteren Blonde","style":"Belgian Pale Ale","ABV":"5.80%","rating":"4.24"},
+			{"brewery":"Brasserie d'Orval S.A.","name":"Orval Trappist Ale","style":"Belgian Pale Ale","ABV":"6.90%","rating":"4.16"},
+			{"brewery":"Brouwerij De Ranke","name":"Cuvee De Ranke","style":"Belgian Pale Ale","ABV":"7.00%","rating":"4.13"},
+			{"brewery":"Russian River Brewing Company","name":"Redemption","style":"Belgian Pale Ale","ABV":"5.15%","rating":"4.06"},
+			{"brewery":"Jolly Pumpkin Artisan Ales","name":"Luciernaga, The Firefly","style":"Belgian Pale Ale","ABV":"6.50%","rating":"4.06"},
+			{"brewery":"New Glarus Brewing Company","name":"R&amp;D Golden Ale","style":"Belgian Pale Ale","ABV":"7.00%","rating":"4.05"},
+			{"brewery":"Brewery Ommegang","name":"Rare Vos (Amber Ale)","style":"Belgian Pale Ale","ABV":"6.50%","rating":"4.02"},
+			{"brewery":"Brouwerij De Musketiers","name":"Troubadour Blond Ale","style":"Belgian Pale Ale","ABV":"6.50%","rating":"4.01"},
+			{"brewery":"Brouwerij De Musketiers","name":"Antigoon By Robert Wiedmaier","style":"Belgian Pale Ale","ABV":"6.80%","rating":"4.01"},
+			{"brewery":"COAST Brewing Company","name":"Belafonte","style":"Belgian Pale Ale","ABV":"5.70%","rating":"4.01"}
+		]},
+		{"style":"Hefeweizen", "details": [
+			{"brewery":"Bayerische Staatsbrauerei Weihenstephan","name":"Weihenstephaner Hefeweissbier","style":"Hefeweizen","ABV":"5.40%","rating":"4.41"},
+			{"brewery":"Live Oak Brewing Company","name":"Live Oak HefeWeizen","style":"Hefeweizen","ABV":"5.20%","rating":"4.36"},
+			{"brewery":"New Glarus Brewing Company","name":"Dancing Man Wheat","style":"Hefeweizen","ABV":"7.20%","rating":"4.3"},
+			{"brewery":"Weisses Brauhaus G. Schneider","name":"Schneider Weisse Tap 1 Mein Blondes","style":"Hefeweizen","ABV":"4.90%","rating":"4.15"},
+			{"brewery":"Klosterbrauerei Andechs","name":"Andechser Weissbier Hefetrub","style":"Hefeweizen","ABV":"5.50%","rating":"4.15"},
+			{"brewery":"Dry Dock Brewing Co.","name":"Dry Dock Hefeweizen","style":"Hefeweizen","ABV":"4.30%","rating":"4.13"},
+			{"brewery":"Privatbrauerei Franz Inselkammer KG","name":"Ayinger Brau Weisse","style":"Hefeweizen","ABV":"5.10%","rating":"4.12"},
+			{"brewery":"Three Floyds Brewing Co.","name":"Drunk Monk Hefeweizen","style":"Hefeweizen","ABV":"5.50%","rating":"4.12"},
+			{"brewery":"Karbach Brewing Co.","name":"Weisse Versa Wheat","style":"Hefeweizen","ABV":"5.20%","rating":"4.1"},
+			{"brewery":"Sierra Nevada Brewing Co.","name":"Sierra Nevada Kellerweis Hefeweizen","style":"Hefeweizen","ABV":"4.80%","rating":"4.09"}
+		]}
+	]
+};
